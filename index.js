12fab8b64df8bcd9ec706a26130e545917b09edb
Khateeb Rehman <junaydkr@gmail.com>
2022-03-04 17:02:52 +0000

    brewery

--- index.js
+++ index.js
@@ -27,19 +27,37 @@ selectStateForm.addEventListener("submit", function (e) {
     });
 });
 
-const brew = {
-    name: selectStateForm.name.value
-}
+// const brew = {
+//     name: selectStateForm.name.value
+// }
 
 function renderBreweries() {
   for (const brewery of state.breweries) {
     const liEl = document.createElement("li");
-    liEl.innerHTML
+    liEl.innerHTML = `<h2>${brewery.name}</h2>
+    <div class="type">${brewery.brewery_type}</div>
+    <section class="address">
+        <h3>Address:</h3>
+        <p>${brewery.street}</p>
+        <p><strong>${brewery.city}, ${brewery.postal_code}</strong></p>
+    </section>
+    <section class="phone">
+        <h3>Phone:</h3>
+        <p>${brewery.phone}</p>
+    </section>
+    <section class="link">
+        <a href=${brewery.website_url} target="_blank">Visit Website</a>
+    </section>`
+    breweriesList.append(liEl)
   }
-
 }
+  
 
 function render() {
   renderBreweries();
-//   clear();
+  clear();
+}
+
+function clear() {
+  liEl.innerHTML = ''
 }
